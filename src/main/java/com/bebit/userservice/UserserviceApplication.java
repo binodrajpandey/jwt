45cbe247cad30a4db8702708dfeb8a523a691872
binodrajpandey <binodraj.pandey@bebit.com>
2021-11-11 20:53:43 +0900

    add rest of the apis and user

--- src/main/java/com/bebit/userservice/UserserviceApplication.java
+++ src/main/java/com/bebit/userservice/UserserviceApplication.java
@@ -1,13 +1,39 @@
 package com.bebit.userservice;
 
+import com.bebit.userservice.domain.AppUser;
+import com.bebit.userservice.domain.Role;
+import com.bebit.userservice.service.UserService;
+import java.util.ArrayList;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UserserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserserviceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(UserService userService) {
+		return args -> {
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_MANAGER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+			userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+			userService.saveUser(new AppUser(null, "Binod", "binod", "1234",new ArrayList<>()));
+			userService.saveUser(new AppUser(null, "Lukas", "lukas", "1234",new ArrayList<>()));
+			userService.saveUser(new AppUser(null, "Anil", "anil", "1234",new ArrayList<>()));
+			userService.saveUser(new AppUser(null, "Shojibur", "shojibur", "1234",new ArrayList<>()));
+
+			userService.addRoleToUser("binod", "ROLE_USER");
+			userService.addRoleToUser("lukas", "ROLE_MANAGER");
+			userService.addRoleToUser("anil", "ROLE_ADMIN");
+			userService.addRoleToUser("shojibur", "ROLE_SUPER_ADMIN");
+		};
 	}
 
 }
--- src/main/java/com/bebit/userservice/api/UserController.java
+++ src/main/java/com/bebit/userservice/api/UserController.java
@@ -1,18 +1,26 @@
 package com.bebit.userservice.api;
 
 import com.bebit.userservice.domain.AppUser;
+import com.bebit.userservice.domain.Role;
 import com.bebit.userservice.service.UserService;
+import java.net.URI;
 import java.util.List;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class UserController {
+
   private final UserService userService;
 
   @GetMapping("/users")
@@ -20,4 +28,38 @@ public class UserController {
     return ResponseEntity.ok().body(userService.getUsers());
   }
 
+  @PostMapping("/user/save")
+  public ResponseEntity<AppUser> saveUser(@RequestBody AppUser appUser) {
+    URI uri = URI.create(
+        ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+    return ResponseEntity.created(uri).body(userService.saveUser(appUser));
+  }
+
+  @GetMapping("/role/save")
+  public ResponseEntity<Role> saveRole(@RequestBody Role role) {
+    URI uri = URI.create(
+        ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/role/save").toUriString());
+
+    return ResponseEntity.created(uri).body(userService.saveRole(role));
+  }
+
+  @GetMapping("/role/addtouser")
+  public ResponseEntity<List<AppUser>> addRoleToUser(
+      @RequestBody RoleToAppUserForm roleToAppUserForm) {
+    userService.addRoleToUser(roleToAppUserForm.getUsername(), roleToAppUserForm.getRoleName());
+    return ResponseEntity.ok().build();
+  }
+
+  @GetMapping("/user/{username}")
+  public ResponseEntity<AppUser> getUser(@PathVariable String username) {
+    return ResponseEntity.ok().body(userService.getUser(username));
+  }
+
+}
+
+@Data
+class RoleToAppUserForm {
+
+  private String username;
+  private String roleName;
 }
